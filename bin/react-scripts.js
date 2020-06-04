83dccf1b8d8abedffaccf7b8e2556659cc9f8d94
1225zhangqian <qian.zhang@hexagon.com>
2020-06-04 10:39:26 +0800

    update createSfxApp.js

---
+++ bin/react-scripts.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+var currentNodeVersion = process.versions.node;
+var semver = currentNodeVersion.split('.');
+var major = semver[0];
+
+if (major < 10) {
+    console.error(
+        'You are running Node ' +
+        currentNodeVersion +
+        '.\n' +
+        'Create SFx App requires Node 10 or higher. \n' +
+        'Please update your version of Node.'
+    );
+    process.exit(1);
+}
+
+require('./try.js');
